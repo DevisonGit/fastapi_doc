28493d44a137e6908b9aebdbd3f4bc818dabe7a6
Devisono <devison_santos@yahoo.com.br>
2026-03-30 11:19:41 -0300

    query parameters models

---
+++ 06_query_parameters_models.py
@@ -0,0 +1,31 @@
+from typing import Annotated, Literal
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+# Query parameters with Pydantic Model
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
+
+
+# Fordib extra query parameters
+class FilterParamsForbid(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    limit: int = Field(100, gt=0, le=100)
+
+
+@app.get("/items/now")
+async def read_items(filter_query: Annotated[FilterParamsForbid, Query()]):
+    return filter_query
