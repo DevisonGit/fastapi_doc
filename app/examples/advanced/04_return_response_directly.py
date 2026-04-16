b036458d30ce7cfecc85ce9e382dc6357a036781
Devisono <devison_santos@yahoo.com.br>
2026-04-16 10:43:37 -0300

    return a response directly

---
+++ app/examples/advanced/04_return_response_directly.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from fastapi import FastAPI, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    title: str 
+    timestamp: datetime
+    description: str | None = None
+
+
+app = FastAPI()
+
+
+# Return a response
+@app.put('/items/{id}')
+async def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+# Return a custom response
+@app.get('/legacy')
+def get_legacy_data():
+    data = """<?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type='application/xml')
