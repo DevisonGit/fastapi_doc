6f0c079dcf02130a02ec0db91b6b00577699e132
Devisono <devison_santos@yahoo.com.br>
2026-04-18 09:44:13 -0300

    Responses, cookies, headers and status code

---
+++ app/examples/advanced/08_response_header.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get('/headers-and-object/')
+async def  get_headers(response: Response):
+    response.headers['X-Cat-Dog'] = 'Alone in the world'
+    return {'message': 'hello world'}
+
+
+@app.get('/headers/')
+def get_headers():
+    content = {'message': 'Hello world'}
+    headers = {'X-cat-dog': 'alone in the world', 'Content-language': 'en-US'}
+    return JSONResponse(content=content,headers=headers)
