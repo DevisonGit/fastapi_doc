909ef4b5fb673bcbc4ca8f7e90efeaab01041c8f
Devisono <devison_santos@yahoo.com.br>
2026-04-25 18:16:27 -0300

    async tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,23 +1,16 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
 from fastapi import status
-from fastapi.testclient import TestClient
 
 from ..main import app
 
-client = TestClient(app)
 
-
-def test_read_main():
-    response = client.get('/?token=jessica')
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get('/')
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {'message': 'hello bigger applications!'}
-
-
-def test_read_main_invalid_token():
-    response = client.get('/?token=jeh')
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {'detail': 'No jessica token provided'}
-
-
-def test_read_main_no_token():
-    response = client.get('/')
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+    assert response.json() == {'message': 'tomato'}
+    
---
+++ app/tests/test_main_v1.py
@@ -0,0 +1,23 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from ..main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get('/?token=jessica')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'message': 'hello bigger applications!'}
+
+
+def test_read_main_invalid_token():
+    response = client.get('/?token=jeh')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {'detail': 'No jessica token provided'}
+
+
+def test_read_main_no_token():
+    response = client.get('/')
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
